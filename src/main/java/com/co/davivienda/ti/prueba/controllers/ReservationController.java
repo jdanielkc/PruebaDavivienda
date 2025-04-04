fa25d0b5a3d2aa2b8eb21b73ad6c292c396304d6
Jose Daniel Garcia Arias <jd.garciaa1@uniandes.edu.co>
2025-04-04 03:22:50 -0500

    Se añaden métodos para obtener reservas de un usuario y una reserva específica por ID en ReservationController, junto con la clase AllReservationsResponse para manejar las respuestas.

--- src/main/java/com/co/davivienda/ti/prueba/controllers/ReservationController.java
+++ src/main/java/com/co/davivienda/ti/prueba/controllers/ReservationController.java
@@ -2,6 +2,7 @@ package com.co.davivienda.ti.prueba.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.co.davivienda.ti.prueba.models.dto.ReservationCreateDTO;
+import com.co.davivienda.ti.prueba.models.response.AllReservationsResponse;
 import com.co.davivienda.ti.prueba.models.response.ReservationResponse;
 import com.co.davivienda.ti.prueba.services.IReservationService;
 
@@ -33,5 +35,18 @@ public class ReservationController {
             @PathVariable String userId,
             @PathVariable Long reservationId) {
         return reservationService.deleteReservation(userId, reservationId);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<AllReservationsResponse> getUserReservations(
+            @PathVariable String userId) {
+        return reservationService.getUserReservations(userId);
+    }
+
+    @GetMapping("/{userId}/{reservationId}")
+    public ResponseEntity<ReservationResponse> getUserReservationById(
+            @PathVariable String userId,
+            @PathVariable Long reservationId) {
+        return reservationService.getUserReservationById(userId, reservationId);
     }
 }
