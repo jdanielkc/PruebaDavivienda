a9f2f4dd6cefe52954921bc98de35adced97d716
Jose Daniel Garcia Arias <jd.garciaa1@uniandes.edu.co>
2025-04-04 03:22:44 -0500

    Se añaden métodos para obtener reservas de un usuario y una reserva específica por ID en IReservationService y ReservationService, incluyendo validaciones de usuario.

--- src/main/java/com/co/davivienda/ti/prueba/repositories/ReservationRepository.java
+++ src/main/java/com/co/davivienda/ti/prueba/repositories/ReservationRepository.java
@@ -1,5 +1,8 @@
 package com.co.davivienda.ti.prueba.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,8 +10,10 @@ import com.co.davivienda.ti.prueba.entities.Reservation;
 
 /**
  * Repository interface for Reservation entity.
- * This interface extends JpaRepository to provide CRUD operations for Reservation.
- * It is annotated with @Repository to indicate that it is a Spring Data repository.
+ * This interface extends JpaRepository to provide CRUD operations for
+ * Reservation.
+ * It is annotated with @Repository to indicate that it is a Spring Data
+ * repository.
  * 
  * @author Jose Daniel Garcia Arias
  * @version 1.0.0
@@ -16,5 +21,7 @@ import com.co.davivienda.ti.prueba.entities.Reservation;
  */
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByUserIdOrderByReservationDateDesc(Long userId);
 
+    Optional<Reservation> findByIdAndUserId(Long reservationId, Long userId);
 }
--- src/main/java/com/co/davivienda/ti/prueba/services/IReservationService.java
+++ src/main/java/com/co/davivienda/ti/prueba/services/IReservationService.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.co.davivienda.ti.prueba.models.dto.ReservationCreateDTO;
+import com.co.davivienda.ti.prueba.models.response.AllReservationsResponse;
 import com.co.davivienda.ti.prueba.models.response.ReservationResponse;
 
 public interface IReservationService {
@@ -14,4 +15,10 @@ public interface IReservationService {
 
     @Transactional
     public ResponseEntity<ReservationResponse> deleteReservation(String userId, Long reservationId);
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<AllReservationsResponse> getUserReservations(String userId);
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<ReservationResponse> getUserReservationById(String userId, Long reservationId);
 }
