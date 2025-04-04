9a2f427a9d54e5a439a5ac54d9af109838382ca0
Jose Daniel Garcia Arias <jd.garciaa1@uniandes.edu.co>
2025-04-04 00:38:53 -0500

    Se implementa la autenticación JWT añadiendo filtros y servicios correspondientes. Se configura la seguridad de la aplicación y se actualiza el archivo pom.xml para incluir dependencias necesarias para JWT.

---
+++ src/main/java/com/co/davivienda/ti/prueba/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.co.davivienda.ti.prueba.security;
+
+import com.co.davivienda.ti.prueba.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var userEntity = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+        
+        return new User(userEntity.getEmail(), userEntity.getPassword(), new ArrayList<>());
+    }
+}
